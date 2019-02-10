33c2d79098a41fe325328bd52f7d98067f31dafa
Yener Oezsoy <yeners94@web.de>
2019-02-10 01:22:38 +0100

    Crawler v1.0

---
+++ GoTourExercise/Crawler.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	var visited []string
+	var mutex = sync.Mutex{}
+	var channels []chan int
+	if depth <= 0 {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		var ch = make(chan int)
+		channels = append(channels, ch)
+		mutex.Lock()
+		visited = append(visited, u)
+		go Crawler(u, depth-1, fetcher, &visited, &mutex, ch, depth-1)
+		mutex.Unlock()
+	}
+
+	for _, ch := range channels {
+		<-ch
+		close(ch)
+	}
+	return
+
+}
+
+func Crawler(url string, depth int, fetcher Fetcher, visited *[]string, mutex *sync.Mutex, ch chan int, endingDepth int) {
+	if depth <= 0 {
+		checkEnd(depth, endingDepth, ch)
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		checkEnd(depth, endingDepth, ch)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		if !contains(u, visited, mutex) {
+			mutex.Lock()
+			*visited = append(*visited, u)
+			mutex.Unlock()
+			Crawler(u, depth-1, fetcher, visited, mutex, ch, endingDepth)
+		}
+
+	}
+	checkEnd(depth, endingDepth, ch)
+
+	return
+}
+
+func checkEnd(depth int, endingDepth int, ch chan int) {
+	if depth == endingDepth {
+		ch <- 1
+	}
+}
+
+func contains(url string, visited *[]string, mutex *sync.Mutex) bool {
+	mutex.Lock()
+	for _, element := range *visited {
+		if element == url {
+			defer mutex.Unlock()
+			return true
+		}
+	}
+	defer mutex.Unlock()
+	return false
+}
+
+func main() {
+	Crawl("http://golang.org/", 4, fetcher)
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"http://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"http://golang.org/pkg/",
+			"http://golang.org/cmd/",
+		},
+	},
+	"http://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/cmd/",
+			"http://golang.org/pkg/fmt/",
+			"http://golang.org/pkg/os/",
+		},
+	},
+	"http://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+	"http://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+}
